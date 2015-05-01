948cb3bc5d63111ad677792e673a3d8cd7f43a1b
kenjones21 <cubsfan@udel.edu>
2015-05-01 15:23:43 -0400

    Added a multiple file hello world program

---
+++ hellofunc.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "hellofunc.h"
+
+void Hello::myPrintHelloWorld() {
+  std::cout << "Hello World! from myPrintHelloWorld" << std::endl;
+  return;
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+// my first program in C++
+#include <iostream>
+#include "hellofunc.h"
+
+int main()
+{
+  Hello myHello;
+  myHello.myPrintHelloWorld();
+  return 0;
+}
