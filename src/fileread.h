5bedbc70c6f0cc84320f03b498acd02d8f0fcbc7
kenjones21 <cubsfan@udel.edu>
2015-05-05 21:10:11 -0400

    Added fileread

---
+++ src/fileread.h
@@ -0,0 +1,26 @@
+#ifndef FILEREAD_H
+#define FILEREAD_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include "protein.h"
+
+class Fileread {
+
+private:
+  
+  std::string pdbFileName;
+  std::string psfFileName;
+  std::vector<Atom> tempAtoms;
+  Protein protein;
+
+public:
+  
+  Fileread(std::string pdbFileName, std::string psfFileName);
+  void initSystem();
+  
+};
+
+#endif
---
+++ src/filread.cpp
@@ -0,0 +1,73 @@
+#include "fileread.h"
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+#include "atom.h"
+#include "residue.h"
+#include "protein.h"
+
+Fileread::Fileread(std::string pdbFileName, std::string psfFileName) {
+  this->pdbFileName = pdbFileName;
+  this->psfFileName = psfFileName;
+}
+
+void Fileread::initSystem() {
+  Protein protein;
+  std::ifstream pdbFile(this->pdbFileName);
+  std::vector<Atom> atoms;
+  std::vector<Residue> residues;
+  if (pdbFile.is_open()) {
+    std::cout << "It's working" << std::endl;
+    std::string line = "";
+    getline(pdbFile, line); // Again, first line useless
+    int count = 2;
+    int resnum;
+    int prevresnum = 1;
+    while (getline(pdbFile, line)) {
+      if (line[77] == ' ' && line[12] != 'H' && line[13] != 'H') {
+        Atom tempAtom;
+        if (line[13] == 'O') {
+          tempAtom.setInfo(8, count - 1, 16.002);
+        }
+        else if (line[13] == 'C') {
+          tempAtom.setInfo(6, count - 1, 12.001);
+        }
+        else if (line[13] == 'N') {
+          tempAtom.setInfo(7, count - 1, 14.007);
+        }
+        if (line[24] == ' ') {
+          resnum = line[25] - '0';
+        }
+        else {
+          std::string resNumString = "";
+          resNumString += line[24];
+          resNumString += line[25];
+          resnum = std::stoi(resNumString);
+        }
+        if (resnum != prevresnum) { // On resnum change
+          Residue res(atoms);
+          protein.add_residue(res);
+          prevresnum = resnum;
+          atoms.clear();
+        }
+        atoms.push_back(tempAtom);
+      }
+      count++;
+    }
+    Residue res(atoms);
+    protein.add_residue(res);
+    std::cout << "Number of residues in protein: " << protein.getNumRes() << std::endl;
+  }
+  else {
+    std::cout << "Error opening PDB File" << std::endl;
+  }
+  std::ifstream psfFile(this->psfFileName);
+  if (psfFile.is_open()) {
+    
+  }
+  else {
+    std::cout << "Error opening PSF File" << std::endl;
+  }
+}
